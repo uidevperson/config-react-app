6b56d862df598d406fdc4c8670f58080ff35f23f
uidevperson <32483505+uidevperson@users.noreply.github.com>
2018-04-25 12:16:16 +0100

    set state

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 class Square extends React.Component {
+   constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={ () => this.setState({value: 'X'}) } >
+        {this.state.value}
       </button>
     );
   }
