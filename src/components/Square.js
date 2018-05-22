15b0014a0a8bd02824340409387f49d71067a00e
uidevperson <32483505+uidevperson@users.noreply.github.com>
2018-05-22 16:16:38 +0100

    Add files via upload

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Square.css';
+// controlled component
 
-const Square = (props) => {
+// functional component layout nodes style
+// const Square = (props) => {
+//   return (
+//     <button className="square" onClick={props.onClick}>
+//       {props.value}
+//     </button>
+//   );
+// };
+
+
+// functional component layout 1
+function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
-      {props.value}
+      {props.value}&nbsp;
     </button>
   );
-};
+}
 
 export default Square;
